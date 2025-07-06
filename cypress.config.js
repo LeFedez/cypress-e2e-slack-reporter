7e0e302b921561d9518818c3dda4960e67d5ddd8
Leonardo Fernandez <leofernandez03@gmail.com>
2025-07-05 21:12:21 -0300

    Fix Cypress supportFile issue

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,8 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}' // Busca los tests en esta ruta
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: false // <-- ¡Añade esta línea!
   },
-  projectId: "your-project-id" // Puedes poner cualquier ID o el tuyo si usas Cypress Dashboard
+  projectId: "your-project-id"
 })
